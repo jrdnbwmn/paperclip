e9d180ae64e6553c0ac758d292a3122f18022497
Jordan Bowman <desk@jrdnbwmn.com>
2018-04-05 20:04:35 -0600

    Change root based on if user is signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-
-  root 'welcome#index'
-
   resources :documents
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'documents#index', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
