976bd6e1803d3296600a8e155e52d4580a6a0faa
Jordan Bowman <desk@jrdnbwmn.com>
2018-04-23 21:06:59 -0600

    Revert "Generate some category stuff"
    This reverts commit 25ab75e6aec6377f3b4fd175eb110e9fe15aaef5.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_021612) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_04_23_214416) do
 
   create_table "documents", force: :cascade do |t|
     t.string "title"
@@ -24,7 +18,6 @@ ActiveRecord::Schema.define(version: 2018_04_24_021612) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
