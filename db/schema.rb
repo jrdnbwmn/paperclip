5d99a4c7360d139a93df933a55693a3c59704792
Jordan Bowman <desk@jrdnbwmn.com>
2018-04-30 17:01:39 -0600

    Get the schema back where it's supposed to be

--- db/schema.rb
+++ db/schema.rb
@@ -28,18 +28,6 @@ ActiveRecord::Schema.define(version: 2018_04_30_220204) do
     t.index ["category_id"], name: "index_documents_on_category_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
