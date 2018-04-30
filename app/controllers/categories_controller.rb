a8f3a111b2d5f8000c78a889f02efdf6e8505351
Jordan Bowman <desk@jrdnbwmn.com>
2018-04-30 15:20:38 -0600

    Set up category pages (#3)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,16 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
+    @documents = Document.where(user_id: current_user)
+    @user = current_user
+    @categories = @user.categories.order(:name)
   end
 
   def show
+    @documents = Document.where(user_id: current_user)
+    @user = current_user
+    @categories = @user.categories.order(:name)
   end
 
   def new
@@ -20,5 +27,16 @@ class CategoriesController < ApplicationController
 
   def destroy
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def browser_title(page_title)
     content_for(:browser_title) { page_title }
   end
+
+  def active_class(link_path)
+    current_page?(link_path) ? "link--highlighted" : ""
+  end
 end
